3c85e2b1b910ab78ff95ffb41ffa5853d32302f8
alwaysssraeee26 <raechatt06@gmail.com>
2025-02-17 03:48:54 -0500

    Submission lab5-6.cpp

--- lab5-6.cpp
+++ lab5-6.cpp
@@ -1,33 +1,33 @@
-//lab5-6.cpp - displays a service charge, which is 1%
-//of the purchase price; however, an additional $5 service
-//charge should be added to code 2 purchases
-//Created/revised by <your name> on <current date>
-
-#include <iostream> 
+#include <iostream>
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-	char code = ' ';
-	double purchase = 0.0;
-	double serviceChg = 0.0;
+int main() {
+    char code;            // Variable to hold the user input for the code
+    double purchase;      // Variable to hold the purchase price
+    double serviceChg;    // Variable to hold the service charge
 
-	//enter input data
-	cout << "Enter code: ";
-	cin >> code;
-	cout << "Purchase price: ";
-	cin >> purchase;
+    // Prompt user for input
+    cout << "Enter code (1, 2, or 3): ";
+    cin >> code;  // Take character input for the code
 
-	//calculate service charge
-	serviceChg = purchase * 0.01;
-	//add $5 to code 2 purchases
-	if (code = '2')
-		serviceChg = serviceChg + 5;
-	//end if
+    // Clear the input buffer to ensure there's no leftover newline character
+    cin.ignore();
 
-	//display service charge
-	cout << fixed << setprecision(2);
-	cout << "Service charge: " << serviceChg << endl;
-	return 0;
-}   //end of main function
+    cout << "Enter purchase price: ";
+    cin >> purchase;  // Take the purchase price input
+
+    // Calculate service charge (1% of the purchase price)
+    serviceChg = purchase * 0.01;
+
+    // If the code is '2', add an additional $5 to the service charge
+    if (code == '2') {
+        serviceChg += 5;  // Add $5 if the code is '2'
+    }
+
+    // Display the service charge with 2 decimal precision
+    cout << fixed << setprecision(2);
+    cout << "Service charge: " << serviceChg << endl;
+
+    return 0;  // End of the program
+}
